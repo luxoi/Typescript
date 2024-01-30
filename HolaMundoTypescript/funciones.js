4c2fe88f953e801ceb03392efb2fe2d86e6419ca
Luis Loyola Cuevas <150284189+luxoi@users.noreply.github.com>
2024-01-30 13:57:18 -0300

    Add files via upload

---
+++ HolaMundoTypescript/funciones.js
@@ -0,0 +1,13 @@
+"use strict";
+let saludo = "holamundo desde typescript";
+let numero = 10;
+let cualquiera;
+cualquiera = "cadena";
+cualquiera = 10;
+const PI = 3.14;
+function saludar() {
+    console.log(saludo);
+    console.log(numero);
+    console.log(PI);
+}
+saludar();
---
+++ HolaMundoTypescript/funciones.ts
@@ -0,0 +1,17 @@
+let saludo = "holamundo desde typescript";
+let numero = 10;
+
+let cualquiera: any;
+cualquiera = "cadena";
+cualquiera = 10;
+
+const PI = 3.14;
+
+
+function saludar(){
+    console.log(saludo);
+    console.log(numero);
+    console.log(PI);
+}
+
+saludar();
